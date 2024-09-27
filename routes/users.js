2e53ea8bd257e192f0b77d56387e7f963a1f9f8f
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-09-27 16:09:45 +0300

    add id

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,10 @@ router.get('/', function (req, res, next) {
   res.send(contacts);
 });
 
-
+router.get('/:id', function (req, res, next) {
+  const { id } = req.params;
+  const contact = contacts.filter(el => el.id === id);
+  res.json(contact);
+});
 
 module.exports = router;

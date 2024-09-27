6626d3c0e6564b936a029c080860906ceaf0d177
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-09-27 13:57:36 +0300

    add commits for code; completed npm i; add nodemon for live reload; created in package.json scripts start:dev

--- app.js
+++ app.js
@@ -13,21 +13,21 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev')); /* блок ПЗ */
+app.use(express.json()); /* блок ПЗ */
+app.use(express.urlencoded({ extended: false })); /* блок ПЗ */
+app.use(cookieParser()); /* блок ПЗ */
+app.use(express.static(path.join(__dirname, 'public'))); /* обработка статических файлов */
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// обработка не существующего роута или 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler сам процесс обработки 404 или не существу.роута
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
